29b324159d5a44c9adfbca5fed313a53f2dae9b0
Ronik <ronikgorasiya@gmail.com>
2024-12-31 09:30:25 +0530

    recot

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
+    const [isopenmenu, setisopenmenu] = useState(false);
+
+    const [scrolled, setScrolled] = useState(false);
+
+    // Track scroll position
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 700) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    const hantogglemenu = () => {
+        setisopenmenu(!isopenmenu);
+    }
+    const links = [
+        { id: 1, labal: "apply now", path: "/aboutpage" },
+        { id: 2, labal: "hire now", path: "/service" },
+        { id: 3, labal: "login", path: "/ourwork" },
+
+    ]
+
     return (
-        <div>Header</div>
+        <>
+            <div
+                className={`fixed top-0 hidden lg:flex left-0 w-full h-auto  z-[99] px-[3vw] transition-all duration-300  '}`}
+            >
+                <div className="w-full">
+                    <div className="relative flex items-center justify-between z-[5]">
+                        <a className="inline-block me-[16px] py-[8px]" href="/">
+                            <img src={'https://resources.workindia.in/employer/assets/icon/workindia-full.png'} className="h-[40px]" alt="Logo" />
+                        </a>
+                        <div className="items-center flex-grow hidden lg:flex">
+                            <ul className="flex flex-row gap-4 ml-auto mr-[3rem]">
+                                {links.map((link, id) => {
+
+                                    return (
+                                        <li className="relative " key={id}>
+                                            <a
+                                                className={` 4xl:text-[20px]  xl:text-[14px] capitalize duration-1000 px-[15px] py-[7px] border border-[#212529] cursor-pointer rounded-md hover:bg-[#212529] hover:text-white  `}
+                                                to={link.path}
+                                            >
+                                                {link.labal}
+                                            </a>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fixed top-0 flex lg:hidden bg-[#f6f6f6] left-0 w-full h-auto z-[99] py-[15px] px-[15px]">
+                <div className="container mx-auto">
+                    <div className="relative flex items-center flex-col justify-between z-[5]">
+                        <div className="flex items-center justify-between w-full">
+                            <a className='inline-block me-[16px] py-[8px]' href="/">
+                                <img src={'https://resources.workindia.in/employer/assets/icon/workindia-full.png'} className='h-[30px]' alt="" />
+                            </a>
+                            <div className="menu">
+                                <FaBars onClick={hantogglemenu} />
+                            </div>
+                        </div>
+                        {isopenmenu && (
+                            <div
+                                className={`flex items-center flex-grow w-full transition-all duration-1000 ease-in-out opacity-100 ${isopenmenu ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+                            >
+                                <ul className='flex flex-col w-full text-center'>
+                                    {links.map((link, id) => {
+
+                                        return (
+                                            <li className='relative py-[7px]' key={id}>
+                                                <a className={`text-[20px] capitalize   duration-1000 my-[5px] mx-[15px] font-medium p-[15px] `} to={link.path}>
+                                                    {link.labal}
+                                                </a>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
